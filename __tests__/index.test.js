7c80849a1a5a15df67da2b02de44e713bad8c241
Vita Chistyakova <vita_avi@yahoo.com>
2018-02-23 17:15:54 +0300

    Fix polimorphism

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,25 @@
 import { readFileSync } from 'fs';
 import genDiff from '../src';
 
-const beforeJson = '__tests__/__fixtures__/before.json';
-const afterJson = '__tests__/__fixtures__/after.json';
-const beforeYml = '__tests__/__fixtures__/before.yml';
-const afterYml = '__tests__/__fixtures__/after.yml';
-const beforeIni = '__tests__/__fixtures__/before.ini';
-const afterIni = '__tests__/__fixtures__/after.ini';
-const resFile = readFileSync('__tests__/__fixtures__/result', 'utf8');
+const beforeFileJson = '__tests__/__fixtures__/before.json';
+const afterFileJson = '__tests__/__fixtures__/after.json';
+const beforeFileYml = '__tests__/__fixtures__/before.yml';
+const afterFileYml = '__tests__/__fixtures__/after.yml';
+const beforeFileIni = '__tests__/__fixtures__/before.ini';
+const afterFileIni = '__tests__/__fixtures__/after.ini';
+const resFileName = '__tests__/__fixtures__/result';
 
 describe('Generate difference', () => {
   it('reading json', () => {
-    expect(genDiff(beforeJson, afterJson)).toEqual(resFile);
+    expect(genDiff(beforeFileJson, afterFileJson)).toEqual(readFileSync(resFileName, 'utf8'));
   });
   it('reading yamls', () => {
-    expect(genDiff(beforeYml, afterYml)).toEqual(resFile);
+    expect(genDiff(beforeFileYml, afterFileYml)).toEqual(readFileSync(resFileName, 'utf8'));
   });
   it('reading inis', () => {
-    expect(genDiff(beforeIni, afterIni)).toEqual(resFile);
+    expect(genDiff(beforeFileIni, afterFileIni)).toEqual(readFileSync(resFileName, 'utf8'));
   });
   it('reading different', () => {
-    expect(genDiff(beforeIni, afterYml)).toEqual(resFile);
+    expect(genDiff(beforeFileIni, afterFileYml)).toEqual(readFileSync(resFileName, 'utf8'));
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,37 +3,44 @@ import { safeLoad } from 'js-yaml';
 import { parse as parseIni } from 'ini';
 import _ from 'lodash';
 
+const getType = (filePath) => {
+  if (filePath.indexOf('.yml') !== -1) { return 'yaml'; }
+  if (filePath.indexOf('.json') !== -1) { return 'json'; }
+  if (filePath.indexOf('.ini') !== -1) { return 'ini'; }
+  return 'unknown';
+};
+
 const makeData = (filePath) => {
-  if (filePath.indexOf('.yml') !== -1) { return { type: 'yaml', data: readFileSync(filePath) }; }
-  if (filePath.indexOf('.json') !== -1) { return { type: 'json', data: readFileSync(filePath) }; }
-  if (filePath.indexOf('.ini') !== -1) { return { type: 'ini', data: readFileSync(filePath, 'utf-8') }; }
-  return { type: 'unknown', data: filePath };
-};
-
-const chooseParser = (obj) => {
-  switch (obj.type) {
-    case 'json':
-      return JSON.parse(obj.data);
-    case 'yaml':
-      return safeLoad(obj.data);
-    case 'ini':
-      return parseIni(obj.data);
-    default:
-      return obj.data;
+  if (getType(filePath) === 'yaml') {
+    return {
+      data: readFileSync(filePath),
+      parse: safeLoad(readFileSync(filePath)),
+    };
   }
+  if (getType(filePath) === 'json') {
+    return {
+      data: readFileSync(filePath),
+      parse: JSON.parse(readFileSync(filePath)),
+    };
+  }
+  if (getType(filePath) === 'ini') {
+    return {
+      data: readFileSync(filePath, 'utf-8'),
+      parse: parseIni(readFileSync(filePath, 'utf-8')),
+    };
+  }
+  return { parse: readFileSync(filePath) };
 };
-
-export const parse = config => chooseParser(makeData(config));
 
 const findDiff = (data1, data2) => {
   const keysFromData1 = Object.keys(data1);
   const keysFromData2 = Object.keys(data2);
   const unionKeys = _.union(keysFromData1, keysFromData2);
   const result = unionKeys.map((el) => {
-    if (!data1.hasOwnProperty.call(data1, el)) {
+    if (!_.has(data1, el)) {
       return `+ ${el}: ${data2[el]}`;
     }
-    if (!data2.hasOwnProperty.call(data2, el)) {
+    if (!_.has(data2, el)) {
       return `- ${el}: ${data1[el]}`;
     }
     if (data1[el] === data2[el]) {
@@ -46,6 +53,7 @@ const findDiff = (data1, data2) => {
 
 const makeOutput = array => `{\n  ${array.join('\n  ')}\n}`;
 
-const genDiff = (fstConfig, sndConfig) => makeOutput(findDiff(parse(fstConfig), parse(sndConfig)));
+const genDiff = (fstConfig, sndConfig) =>
+  makeOutput(findDiff(makeData(fstConfig).parse, makeData(sndConfig).parse));
 
 export default genDiff;
