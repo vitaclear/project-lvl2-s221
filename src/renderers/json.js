a4eab16bfaf43d06a5c27248fa77fbe681411f0d
Vita Chistyakova <vita_avi@yahoo.com>
2018-02-25 10:16:50 +0300

    Added json-output function

--- src/renderers/json.js
+++ src/renderers/json.js
@@ -1,37 +1,44 @@
-import _ from 'lodash';
-
 const render = (ast, lvl = 0) => {
   const indent = '  ';
   const closeIndent = indent.repeat(lvl);
   const lvlIndent = indent.repeat(lvl + 1);
 
   const stringifyValue = (value, level) => {
-    const realIndent = indent.repeat(level);
-    const longIndent = indent.repeat(level + 2);
+    const startIndent = indent.repeat(level);
+    const longIndent = indent.repeat(level + 1);
     if (value instanceof Object) {
       const keys = Object.keys(value);
       const paires = keys.map(el => `${el}: ${value[el]}`);
-      return `{\n${longIndent}${paires.join(`\n${longIndent}`)}\n${realIndent}}`;
+      return `{\n${longIndent}${paires.join(`\n${longIndent}`)}\n${startIndent}}`;
     }
     return `${value}`;
   };
 
+  const stringifyNode = (type, values, level) => {
+    const startIndent = indent.repeat(level);
+    const longIndent = indent.repeat(level + 1);
+    const typeString = `${longIndent}type: ${type}\n`;
+    const keys = Object.keys(values);
+    const paires = keys.map(el => `${el}: ${stringifyValue(values[el], level + 1)}`);
+    return `{\n${typeString}${longIndent}${paires.join(`\n${longIndent}`)}\n${startIndent}}`;
+  };
+
   const result = ast.map((el) => {
     if (el.type === 'nested') {
-      return `${indent}${el.key}: ${render(el.children, lvl + 2)}`;
+      return `${el.key}: ${render(el.children, lvl + 1)}`;
     }
     if (el.type === 'added') {
-      return `+ ${el.key}: ${stringifyValue(el.afterValue, lvl + 2)}`;
+      return `${el.key}: ${stringifyNode(el.type, { value: el.afterValue }, lvl + 1)}`;
     }
     if (el.type === 'removed') {
-      return `- ${el.key}: ${stringifyValue(el.beforeValue, lvl + 2)}`;
+      return `${el.key}: ${stringifyNode(el.type, { value: el.beforeValue }, lvl + 1)}`;
     }
     if (el.type === 'unchanged') {
-      return `${indent}${el.key}: ${stringifyValue(el.beforeValue, lvl + 2)}`;
+      return `${el.key}: ${stringifyNode(el.type, { value: el.beforeValue }, lvl + 1)}`;
     }
-    return [`+ ${el.key}: ${stringifyValue(el.afterValue, lvl + 2)}`, `- ${el.key}: ${stringifyValue(el.beforeValue, lvl + 2)}`];
+    return `${el.key}: ${stringifyNode(el.type, { newValue: el.afterValue, previousValue: el.beforeValue }, lvl + 1)}`;
   });
-  return `{\n${lvlIndent}${_.flatten(result).join(`\n${lvlIndent}`)}\n${closeIndent}}`;
+  return `{\n${lvlIndent}${result.join(`\n${lvlIndent}`)}\n${closeIndent}}`;
 };
 
 export default render;
